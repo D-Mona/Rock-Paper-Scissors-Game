1195ce0dd562e7d6f316aeaf8ee3753b7140208b
D-Mona <137803587+D-Mona@users.noreply.github.com>
2023-12-03 21:03:33 +0000

    add DOM reference variables

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// DOM References.
+// Images.
+const firstImage = document.getElementById("first-image");
+const secondImage = document.getElementById("second-image");
+const thirdImage = document.getElementById("third-image");
+
+// Displays
+const upperDisplay = document.getElementById("upper-display");
+const lowerDisplay = document.getElementById("lower-display");
+
+// Buttons
+const scissorsBtn = document.getElementById("scissors-button");
+const paperBtn = document.getElementById("paper-button");
+const rockBtn = document.getElementById("rock-button");
+const playerTurnBtn = document.getElementById("player-turn-button");
+const computerTurnBtn = document.getElementById("computer-turn-button");
+const tryAgainBtn = document.getElementById("try-again-button");
+const enterNameBtn = document.getElementById("enter-name-button");
+const playerChoiceContainer = document.getElementById(
+  "player-choice-container"
+);
